97f5043974f08af6028fa61ba9ceb44f2db85ce0
Alan Eduardo Arriaga Martínez <A14N.arriaga@gmail.com>
2021-10-05 16:26:51 -0500

    Modal to add and account implemented

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,10 @@ import { IoMdAddCircle } from "react-icons/io";
 import Header from "./Header";
 import GeneralBalance from "./GeneralBalance";
 import Account from "./Account";
+import AddAccount from "./modals/AddAccount";
 
 const App = () => {
+  const [addAccount, setAddAccount] = useState(false);
   const [showAmount, setShowAmount] = useState(true);
   const hiddeAmout = "----";
 
@@ -13,8 +15,8 @@ const App = () => {
     <>
       <Header />
       <GeneralBalance
-        balance={2500.50}
-				currency={"MXN"}
+        balance={2500.5}
+        currency={"MXN"}
         showAmount={showAmount}
         setShowAmount={setShowAmount}
         hiddeAmout={hiddeAmout}
@@ -23,28 +25,37 @@ const App = () => {
         <div className="card-header">
           <p className="title">Mis cuentas</p>
           <div className="card-options">
-            <IoMdAddCircle className="add" />
+            <IoMdAddCircle className="add" onClick={() => setAddAccount((account: boolean) => account ? false : true)} />
           </div>
         </div>
         <Account
-					bank={"wallet"}
+          bank={"wallet"}
           name={"Cartera"}
           type={"Cuenta corriente"}
           currency={"MXN"}
           amount={100.01}
-					showAmount={showAmount}
-					hiddeAmout={hiddeAmout}
+          showAmount={showAmount}
+          hiddeAmout={hiddeAmout}
         />
-				<Account
-					bank={"banamex"}
+        <Account
+          bank={"banamex"}
           name={"Debito"}
           type={"Cuenta corriente"}
           currency={"MXN"}
           amount={500.01}
-					showAmount={showAmount}
-					hiddeAmout={hiddeAmout}
+          showAmount={showAmount}
+          hiddeAmout={hiddeAmout}
         />
       </section>
+      <section className="card">
+        <div className="card-header">
+          <p className="title">Tarjetas de credito</p>
+          <div className="card-options">
+            <IoMdAddCircle className="add" />
+          </div>
+        </div>
+      </section>
+      {addAccount && <AddAccount setAddAccount={setAddAccount}/>}
     </>
   );
 };
